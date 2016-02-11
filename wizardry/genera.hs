90accf60149cf0b4eb92abb88646526f8465c7ad
JoseD92 <jose_daniel_d@hotmail.com>
2016-02-11 16:13:07 -0430

    programa en haskell que genera casos de prueba

---
+++ wizardry/genera.hs
@@ -0,0 +1,22 @@
+import System.Random
+import System.Environment
+
+isInCircle :: (Double,Double) -> Double -> (Double,Double) -> Bool
+isInCircle (xc,yc) r (x,y) = (x - xc)**2 + (y - yc)**2 <= r**2
+
+separa divisor x = (filter divisor x, filter (not.divisor) x)
+
+generarPatrones (maxX,maxY) number divisor gen gen2 =
+  (take number yes, take number nos)
+  where
+    coords = zip (randomRs (0,maxX) gen) (randomRs (0,maxY) gen2)
+    (yes,nos) = separa divisor coords
+
+main = do
+  g1 <- getStdGen
+  g2 <- newStdGen
+  (x:_) <- getArgs
+  let circle = isInCircle (10,10) 7
+  let (yes,nos) = generarPatrones (20,20) (read x) circle g1 g2
+  putStr $ unlines $ map (\(x,y) -> (show x) ++ " " ++ (show y) ++ " -1") yes
+  putStr $ unlines $ map (\(x,y) -> (show x) ++ " " ++ (show y) ++ " 1") nos
