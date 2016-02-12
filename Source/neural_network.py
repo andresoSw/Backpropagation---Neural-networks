06d33636ee1e1a6adadb758646e00828efe54fa9
Gamar <gamarazuaje@gmail.com>
2016-02-12 06:32:42 -0800

    Neural network now functional

--- Source/neural_network.py
+++ Source/neural_network.py
@@ -6,96 +6,83 @@ def sigmoid(x):
 def sigmoid_derivate(x):
 	return x*(1.0-x)
 
-def backpropagation(X,T,eta,n_in,n_hidden,n_out):
-	# Create feed-forward network with n_in, n_out, n_hidden
-	# Initialize weights to small value
-
-	weights = []
-	layers = [n_in,n_hidden,n_out]
-
-	# Weights from input layer to hidden layer
-	syn0= (-0.05)*np.random.random((n_in,n_hidden))+0.05
-	b1 = (-0.05) *np.random.random() + 0.05
-
-	# Weights from hidden layer to output layer
-	syn1 = (-0.05)*np.random.random((n_hidden,n_out))+0.05
-	b2 = (-0.05) *np.random.random() + 0.05
-
-
-	# print syn1
-	# print syn0
-
-	for i in range(1):
-		for k in range(0,len(X)):
-			print X[k]
-
-	for i in range(1):
-
-		#feed forward
-		l1 = sigmoid(np.dot(X,syn0))
-		l2 = sigmoid(np.dot(l1,syn1))
-		print "bp output"
-		print l2
-
-		l2_error =  T - l2
-		l2_delta = l2_error*sigmoid_derivate(l2)
-
-		l1_error = l2_delta.dot(syn1.T)
-		l1_delta = l1_error*sigmoid_derivate(l1)
-
-		syn1 += eta*l1.T.dot(l2_delta)
-		syn0 += eta*X.T.dot(l1_delta)
-
 class NeuralNework:
 
 	def __init__(self,eta,n_in,n_hidden,n_out):
+		# learning factor
 		self.eta = eta
 		self.n_in = n_in
 		self.n_hidden = n_hidden
 		self.n_out = n_out
+
 		self.initialize_weights()
 
 	def initialize_weights(self):
-		self.W1 = (-0.05)*np.random.random((self.n_in,self.n_hidden))+0.05
-		self.b1 = (-0.05)*np.random.random()+0.05	
-		self.W2 = (-0.05)*np.random.random((self.n_hidden,self.n_out))+0.05
-		self.b2 = (-0.05)*np.random.random()+0.05
+		# Weights from input unit to hidden unit
+		self.W1 = (0.1)*np.random.random((self.n_in,self.n_hidden))-0.5
+		# Activation threshold on hidden layer
+		self.hidden_threshold = (0.1)*np.random.random((self.n_hidden,1))-0.5
+
+		# Weights from hidden unit to output unit
+		self.W2 = (0.1)*np.random.random((self.n_hidden,self.n_out))-0.5
+		# Activation threshold on output layer
+		self.output_threshold = (0.1)*np.random.random((self.n_out,1))-0.5
+
 
 	def feed_forward(self,X):
 		# output of input layer fed with input values
-		self.a1 = sigmoid(np.dot(X,self.W1)+self.b1)
-		print self.a1
+		self.a1 = sigmoid(np.dot(X,self.W1))
 		# output of hidden layer fed with output of input layer
-		self.y = sigmoid(np.dot(self.a1,self.W2)+self.b2)	
+		self.y = sigmoid(np.dot(self.a1,self.W2))		
+		return self.y	
 
+	# backpropagation updating weights with a single example
 	def backpropagate(self,X,T):
-		self.feed_forward(X)
+		for i in range(0,len(X)):
+			x = np.atleast_2d(X[i])
+			t = np.atleast_2d(T[i])
+			self.feed_forward(x)
 
+			output_error = t - self.y
+			output_delta = output_error*sigmoid_derivate(self.y)
+
+			hidden_error = output_delta.dot(self.W2.T)
+			hidden_delta = hidden_error*sigmoid_derivate(self.a1)
+
+			self.W2 += self.eta*self.a1.T.dot(output_delta)
+			self.W1 += self.eta*x.T.dot(hidden_delta)
+
+	# backpropagation updating weights with all examples
+	def backpropagate_batch(self,X,T):
+
+		self.feed_forward(X)
 		# error in output layer
 		output_error = T - self.y
-		output_delta = output_error*sigmoid_derivate(y)
+		output_delta = output_error*sigmoid_derivate(self.y)
 
+		# error in hidden layer
 		hidden_error = output_delta.dot(self.W2.T)
 		hidden_delta = hidden_error*sigmoid_derivate(self.a1)
 
-		self.W1 += self.eta*self.a1.T.dot(output_delta)
-		self.W2 += self.eta*X.T.dot(hidden_delta)
+		# updating weigths
+		self.W2 += self.eta*self.a1.T.dot(output_delta)
+		self.W1 += self.eta*X.T.dot(hidden_delta)
 
-	# 
+	# backpropagation for a number of N iteration
 	def backpropagation(self,X,T,N):
 		for i in range(0,N):
-			self.backpropagate(X,T)
+			self.backpropagate_batch(X,T)
 
 if __name__ == '__main__':
 	
+	np.random.seed(1)
+
 	x = np.array([[0,0],[0,1],[1,0],[1,1]])
-	y = np.array([[0],[1],[1],[1]])
-	backpropagation(x,y,0.05,2,2,1)
+	y = np.array([[0],[1],[1],[0]])
 
-	nn = NeuralNework(0.05,2,2,1)
-
+	nn = NeuralNework(0.7,2,8,1)
+	# Train network with a given number of iterations
 	nn.backpropagation(x,y,10000)
 
+	# Network result after training
 	print nn.feed_forward(x)
-
-	pass
